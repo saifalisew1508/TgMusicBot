20dd2212d271e95badb0f828c8cebf0c9fa0138c
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-05 21:17:20 +0530

    Update __main__.py

--- MissCutie/__main__.py
+++ MissCutie/__main__.py
@@ -214,7 +214,7 @@ async def initiate_bot():
             await LOG_CLIENT.join_chat("MissCutie_Support")
         except:
             pass
-    console.print(f"└[red] Yukki Music Bot Boot Completed.")
+    console.print(f"└[red] MissCutie Music Bot Boot Completed.")
     if STRING1 != "None":
         await pytgcalls1.start()
     if STRING2 != "None":
