a731d8dfa4e915f7501f366f97114de7f38fb4de
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-06 07:42:56 +0530

    Update Download.py

--- MissCutie/Plugins/Download.py
+++ MissCutie/Plugins/Download.py
@@ -129,7 +129,7 @@ async def boom(_, CallbackQuery):
 👀**Views:** `{views}`
 🎥**Channel Name:** {channel}
 🔗**Video Link:** [Link]({yturl})
-👤**Requested by:** {mention}
+👤**Requested by:** {}
 
 ⚡️ __Downloaded By {MUSIC_BOT_NAME}__"""
     filext = "%(title)s.%(ext)s"
